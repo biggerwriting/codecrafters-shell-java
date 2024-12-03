9e79f772c3c95a7e668481516656e70ea452ac22
tongqianwen <tongqianwen-it@bestpay.com.cn>
2024-12-03 19:18:29 +0800

    The cd builtin: Relative paths

--- src/main/java/command/Cd.java
+++ src/main/java/command/Cd.java
@@ -3,27 +3,54 @@ package command;
 import java.io.File;
 import java.nio.file.Files;
 
+import static command.Pwd.pwd;
+
 /**
  * $ cd /does_not_exist
  * cd: /does_not_exist: No such file or directory
+ *
  * @Author: tongqianwen
  * @Date: 2024/12/3
  */
-public class Cd implements Strategy{
+public class Cd implements Strategy {
     @Override
     public String command(String input) {
-
         String[] tokens = input.split(" ");
-        if(tokens.length>1){
+        if (tokens.length > 1) {
             String dir = tokens[1];
-            File file = new File(dir);
-            if(file.exists() && Files.isDirectory(file.toPath())){
-                Pwd.pwd = file.getAbsolutePath();
-                return null;
-            }else {
-                return String.format("cd: %s: No such file or directory", dir);
+            if (dir.startsWith(File.separator)) {
+                return checkDir(dir);
+            } else if (dir.startsWith("." + File.separator)) {
+                dir = pwd + dir.substring(1);
+                // System.out.println("相对路径dir:" + dir);
+                return checkDir(dir);
+            } else if (dir.startsWith(".." + File.separator)) {
+                String[] split = dir.split("\\.\\.");
+                File moveTo = new File(pwd);
+
+                for (int i = 0; i < split.length; i++) {
+                    //System.out.println(i + " " + split[i]);
+                    String current = split[i];
+                    if("".equals(current) || File.separator.equals(current)){
+                        moveTo = moveTo.getParentFile();
+                    }else {
+                        moveTo = new File(moveTo.getAbsolutePath()+current);
+                    }
+                }
+                return checkDir(moveTo.getAbsolutePath());
             }
         }
         return null;
+    }
+
+    private static String checkDir(String dir) {
+        File file = new File(dir);
+        if (file.exists() && Files.isDirectory(file.toPath())) {
+            //System.out.println("pwd is :" + pwd);
+            pwd = file.getAbsolutePath();
+            return null;
+        } else {
+            return String.format("cd: %s: No such file or directory", dir);
+        }
     }
 }
