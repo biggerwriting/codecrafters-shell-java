0932a62a7a88ecd6a9213f40280881d9c2391aad
tongqianwen <tongqianwen-it@bestpay.com.cn>
2024-12-03 18:20:05 +0800

    Run a program

--- src/main/java/command/LocalComand.java
+++ src/main/java/command/LocalComand.java
@@ -19,9 +19,6 @@ public class LocalComand implements Strategy{
         try {
             String[] tokens = input.split(" ");
             tokens[0] = dir;
-
-            System.out.println("local command input:"+input);
-            System.out.println("local command dir:"+dir);
             Process process = Runtime.getRuntime().exec(tokens);
             process.getInputStream().transferTo(System.out);
         } catch (IOException e) {
