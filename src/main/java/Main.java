958331dcf6a6ce8efd6efcbf1af181675ab36d03
tongqianwen <tongqianwen-it@bestpay.com.cn>
2024-12-03 12:52:19 +0800

    REPL (Read-Eval-Print Loop).

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
-
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+        String input;
+        while (!"exit".equalsIgnoreCase((input = scanner.nextLine()))) {
+            System.out.println(input + ": command not found");
+        }
+
     }
 }
