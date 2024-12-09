04157166ab5ab8d638b69332d48588bc25269e08
tongqianwen <tongqianwen-it@bestpay.com.cn>
2024-12-09 11:35:50 +0800

    fix: echo 'script test'

--- src/main/java/command/Echo.java
+++ src/main/java/command/Echo.java
@@ -1,12 +1,15 @@
 package command;
 
+import static util.StringUtils.parseCommand;
+
 /**
  * @Author: tongqianwen
  * @Date: 2024/12/3
  */
 public class Echo implements Strategy {
     @Override
-    public String command(String commands) {
-        return commands.substring(4).trim();
+    public String command(String input) {
+        String[] tokens = parseCommand(input);
+        return tokens[1];
     }
 }
